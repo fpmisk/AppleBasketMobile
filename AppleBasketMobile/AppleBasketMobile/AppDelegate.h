26ca7e574b2f85ea32990e912fbc2010a237e059
Admin <admin@if-you-see-this-then-your-dns-resolver-is-wrong.telecom.by>
2015-09-18 22:12:00 +0300

    Initial Commit

---
+++ AppleBasketMobile/AppleBasketMobile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppleBasketMobile
+//
+//  Created by Admin on 18.09.15.
+//  Copyright (c) 2015 bsu.fpmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
